db62437d16f8c292fb801d93db8e1a82691a71c4
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-11-04 19:32:37 -0500

    adds typewriter effect

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,17 @@
+let tagLine = document.querySelector('[data-tagLine]');
+let i = 0;
+let textArray = ['Software Developer', 'Musician', 'Leader', 'Creator'];
+let txt = 'Software Developer'; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
 
+function typeWriter() {
+    if (i < txt.length) {
+        tagLine.textContent += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
 
+typeWriter();
 
 
