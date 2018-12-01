d3b70a5d729dc62defdaaa59862170b40f8de441
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-01 15:55:06 -0500

    adds new images and text

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-let tagLine = document.querySelector('[data-tagLine]');
+let tagLine = document.querySelector("[data-tagLine]");
 let i = 0;
-let textArray = ['Software Developer', 'Musician', 'Leader', 'Creator'];
-let txt = 'Software Developer'; /* The text */
+let textArray = ["Software Developer", "Musician", "Leader", "Creator"];
+let txt = "Software Developer"; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -14,29 +14,38 @@ function typeWriter() {
 
 typeWriter();
 
-const header = document.querySelector('[data-header]');
-const uMoodImg = document.getElementById('uMoodImg');
+const header = document.querySelector("[data-header]");
+const uMoodImg = document.getElementById("uMoodImg");
+const budgetImg = document.getElementById("budgetImg");
+const psImg = document.getElementById("psImg");
 
-const hamburger = document.querySelector('[data-hamburger]');
-const mobileNav = document.querySelector('[data-mobile-nav]');
+const hamburger = document.querySelector("[data-hamburger]");
+const mobileNav = document.querySelector("[data-mobile-nav]");
 
-hamburger.addEventListener('click', () => {
-  mobileNav.classList.toggle('hidden');
-})
+hamburger.addEventListener("click", () => {
+  mobileNav.classList.toggle("hidden");
+});
 
+uMoodImg.addEventListener("mouseover", () => {
+  uMoodImg.src = "https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif";
+});
 
-uMoodImg.addEventListener('mouseover', () => {
-  uMoodImg.src = 'https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif';
-})
+uMoodImg.addEventListener("mouseout", () => {
+  uMoodImg.src = "../images/desktopHome.png";
+});
 
-uMoodImg.addEventListener('mouseout', () => {
-  uMoodImg.src = '../images/desktopHome.png';
-})
+// budgetImg.addEventListener("mouseover", () => {
+//   budgetImg.src = "";
+// });
 
-psImg.addEventListener('mouseover', () => {
-  psImg.src = 'https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif';
-})
+// budgetImg.addEventListener("mouseout", () => {
+//   budgetImg.src = "../images/Landingimg.png";
+// });
 
-psImg.addEventListener('mouseout', () => {
-  psImg.src = '../images/desktopLanding.png';
-})
+psImg.addEventListener("mouseover", () => {
+  psImg.src = "https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif";
+});
+
+psImg.addEventListener("mouseout", () => {
+  psImg.src = "../images/desktopLanding.png";
+});
