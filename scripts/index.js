19b52891c01fa8c13676ac08b7fcfcd44d4c4adb
Lorenzo Salvio <salviolorenzo@gmail.com>
2018-12-13 13:45:23 -0500

    adds scrollIntoView

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-let tagLine = document.querySelector("[data-tagLine]");
+let tagLine = document.querySelector('[data-tagLine]');
 let i = 0;
-let textArray = ["Software Developer", "Musician", "Leader", "Creator"];
-let txt = "Software Developer"; /* The text */
+let textArray = ['Software Developer', 'Musician', 'Leader', 'Creator'];
+let txt = 'Software Developer'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -14,24 +14,35 @@ function typeWriter() {
 
 typeWriter();
 
-const header = document.querySelector("[data-header]");
-const uMoodImg = document.getElementById("uMoodImg");
-const budgetImg = document.getElementById("budgetImg");
-const psImg = document.getElementById("psImg");
+const header = document.querySelector('[data-header]');
+const uMoodImg = document.getElementById('uMoodImg');
+const budgetImg = document.getElementById('budgetImg');
+const psImg = document.getElementById('psImg');
 
-const hamburger = document.querySelector("[data-hamburger]");
-const mobileNav = document.querySelector("[data-mobile-nav]");
+const home = document.querySelector('[data-home]');
+const homeLink = document.querySelector('[data-home-link]');
+const aboutMe = document.querySelector('[data-about-me]');
+const aboutMeLink = document.querySelector('[data-about-me-link]');
+const projects = document.querySelector('[data-projects]');
+const projectsLink = document.querySelector('[data-projects-link]');
+const skills = document.querySelector('[data-skills]');
+const skillsLink = document.querySelector('[data-skills-link]');
+const contact = document.querySelector('[data-contact]');
+const contactLink = document.querySelector('[data-contact-link]');
 
-hamburger.addEventListener("click", () => {
-  mobileNav.classList.toggle("hidden");
+const hamburger = document.querySelector('[data-hamburger]');
+const mobileNav = document.querySelector('[data-mobile-nav]');
+
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('hidden');
 });
 
-uMoodImg.addEventListener("mouseover", () => {
-  uMoodImg.src = "https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif";
+uMoodImg.addEventListener('mouseover', () => {
+  uMoodImg.src = 'https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif';
 });
 
-uMoodImg.addEventListener("mouseout", () => {
-  uMoodImg.src = "../images/desktopHome.png";
+uMoodImg.addEventListener('mouseout', () => {
+  uMoodImg.src = '../images/desktopHome.png';
 });
 
 // budgetImg.addEventListener("mouseover", () => {
@@ -42,10 +53,26 @@ uMoodImg.addEventListener("mouseout", () => {
 //   budgetImg.src = "../images/Landingimg.png";
 // });
 
-psImg.addEventListener("mouseover", () => {
-  psImg.src = "https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif";
+psImg.addEventListener('mouseover', () => {
+  psImg.src = 'https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif';
 });
 
-psImg.addEventListener("mouseout", () => {
-  psImg.src = "../images/desktopLanding.png";
+psImg.addEventListener('mouseout', () => {
+  psImg.src = '../images/desktopLanding.png';
+});
+
+homeLink.addEventListener('click', () => {
+  home.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+aboutMeLink.addEventListener('click', () => {
+  aboutMe.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+projectsLink.addEventListener('click', () => {
+  projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+skillsLink.addEventListener('click', () => {
+  skills.scrollIntoView({ behavior: 'smooth', block: 'start' });
+});
+contactLink.addEventListener('click', () => {
+  contact.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
