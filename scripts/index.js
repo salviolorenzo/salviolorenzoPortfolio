af3ebf5b9bb96ca137d94e794b7759bc27fa50ab
salviolorenzo <salviolorenzo@gmail.com>
2021-06-15 08:33:02 -0400

    update tagline

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
-let tagLine = document.querySelector("[data-tagLine]");
+let tagLine = document.querySelector('[data-tagLine]');
 let i = 0;
-let textArray = ["Software Developer", "Musician", "Leader", "Creator"];
-let txt = "Software Developer"; /* The text */
+let textArray = ['Software Developer', 'Musician', 'Leader', 'Creator'];
+let txt = 'Software Engineer'; /* The text */
 var speed = 100; /* The speed/duration of the effect in milliseconds */
 
 const typeWriter = () => {
@@ -12,12 +12,12 @@ const typeWriter = () => {
   }
 };
 
-const carouselDiv = document.querySelector("[data-carousel]");
+const carouselDiv = document.querySelector('[data-carousel]');
 
-const images = ["IMG_0289.jpg", "IMG_0311.jpeg", "IMG_0315.jpeg", "paris.jpg"];
+const images = ['IMG_0289.jpg', 'IMG_0311.jpeg', 'IMG_0315.jpeg', 'paris.jpg'];
 const carousel = () => {
   let count = 0;
-  window.setInterval(function() {
+  window.setInterval(function () {
     if (count < 3) {
       carouselDiv.style.backgroundImage = `url(./images/${images[count]})`;
       count++;
@@ -52,11 +52,11 @@ const carousel = () => {
 
 const colorPicker = () => {
   const colors = [
-    "rgba(46, 147, 241, 0.95)",
-    "rgba(94, 172, 245, 0.95)",
-    "rgba(125, 182, 236, 0.95)",
-    "rgba(157, 197, 235, 0.95)",
-    "rgba(199, 219, 238, 0.95)"
+    'rgba(46, 147, 241, 0.95)',
+    'rgba(94, 172, 245, 0.95)',
+    'rgba(125, 182, 236, 0.95)',
+    'rgba(157, 197, 235, 0.95)',
+    'rgba(199, 219, 238, 0.95)',
   ];
 
   let index = Math.floor(Math.random() * colors.length);
@@ -69,37 +69,37 @@ const colorPicker = () => {
 typeWriter();
 carousel();
 
-const header = document.querySelector("[data-header]");
-const uMoodImg = document.getElementById("uMoodImg");
-const budgetImg = document.getElementById("budgetImg");
-const psImg = document.getElementById("psImg");
+const header = document.querySelector('[data-header]');
+const uMoodImg = document.getElementById('uMoodImg');
+const budgetImg = document.getElementById('budgetImg');
+const psImg = document.getElementById('psImg');
 
-const home = document.querySelector("[data-home]");
-const homeLink = document.querySelector("[data-home-link]");
-const aboutMe = document.querySelector("[data-about-me]");
-const aboutMeLink = document.querySelector("[data-about-me-link]");
-const projects = document.querySelector("[data-projects]");
-const projectsLink = document.querySelector("[data-projects-link]");
-const skills = document.querySelector("[data-skills]");
-const skillsLink = document.querySelector("[data-skills-link]");
-const contact = document.querySelector("[data-contact]");
-const contactLink = document.querySelector("[data-contact-link]");
+const home = document.querySelector('[data-home]');
+const homeLink = document.querySelector('[data-home-link]');
+const aboutMe = document.querySelector('[data-about-me]');
+const aboutMeLink = document.querySelector('[data-about-me-link]');
+const projects = document.querySelector('[data-projects]');
+const projectsLink = document.querySelector('[data-projects-link]');
+const skills = document.querySelector('[data-skills]');
+const skillsLink = document.querySelector('[data-skills-link]');
+const contact = document.querySelector('[data-contact]');
+const contactLink = document.querySelector('[data-contact-link]');
 
-const hamburger = document.querySelector("[data-hamburger]");
-const mobileNav = document.querySelector("[data-mobile-nav]");
-const techItems = document.getElementsByClassName("tech-item");
-const iconsList = document.getElementsByClassName("container");
+const hamburger = document.querySelector('[data-hamburger]');
+const mobileNav = document.querySelector('[data-mobile-nav]');
+const techItems = document.getElementsByClassName('tech-item');
+const iconsList = document.getElementsByClassName('container');
 
-hamburger.addEventListener("click", () => {
-  mobileNav.classList.toggle("hidden");
+hamburger.addEventListener('click', () => {
+  mobileNav.classList.toggle('hidden');
 });
 
-uMoodImg.addEventListener("mouseover", () => {
-  uMoodImg.src = "https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif";
+uMoodImg.addEventListener('mouseover', () => {
+  uMoodImg.src = 'https://media.giphy.com/media/YAXIGsoPJojzlVcbJP/giphy.gif';
 });
 
-uMoodImg.addEventListener("mouseout", () => {
-  uMoodImg.src = "../images/desktopHome.png";
+uMoodImg.addEventListener('mouseout', () => {
+  uMoodImg.src = '../images/desktopHome.png';
 });
 
 // budgetImg.addEventListener("mouseover", () => {
@@ -110,57 +110,57 @@ uMoodImg.addEventListener("mouseout", () => {
 //   budgetImg.src = "../images/Landingimg.png";
 // });
 
-psImg.addEventListener("mouseover", () => {
-  psImg.src = "https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif";
+psImg.addEventListener('mouseover', () => {
+  psImg.src = 'https://media.giphy.com/media/3d19VItGVqFZvfDMrd/giphy.gif';
 });
 
-psImg.addEventListener("mouseout", () => {
-  psImg.src = "../images/desktopLanding.png";
+psImg.addEventListener('mouseout', () => {
+  psImg.src = '../images/desktopLanding.png';
 });
 
-ReactImg.addEventListener("mouseover", () => {
-  ReactImg.src = "https://media.giphy.com/media/3d8mZsqNWY3ut85M2L/giphy.gif";
+ReactImg.addEventListener('mouseover', () => {
+  ReactImg.src = 'https://media.giphy.com/media/3d8mZsqNWY3ut85M2L/giphy.gif';
 });
 
-ReactImg.addEventListener("mouseout", () => {
-  ReactImg.src = "../images/ReactProj.png";
+ReactImg.addEventListener('mouseout', () => {
+  ReactImg.src = '../images/ReactProj.png';
 });
 
-homeLink.addEventListener("click", () => {
-  home.scrollIntoView({ behavior: "smooth", block: "start" });
+homeLink.addEventListener('click', () => {
+  home.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
-aboutMeLink.addEventListener("click", () => {
-  aboutMe.scrollIntoView({ behavior: "smooth", block: "center" });
+aboutMeLink.addEventListener('click', () => {
+  aboutMe.scrollIntoView({ behavior: 'smooth', block: 'center' });
 });
-projectsLink.addEventListener("click", () => {
-  projects.scrollIntoView({ behavior: "smooth", block: "start" });
+projectsLink.addEventListener('click', () => {
+  projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
-skillsLink.addEventListener("click", () => {
-  skills.scrollIntoView({ behavior: "smooth", block: "start" });
+skillsLink.addEventListener('click', () => {
+  skills.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
-contactLink.addEventListener("click", () => {
-  contact.scrollIntoView({ behavior: "smooth", block: "start" });
+contactLink.addEventListener('click', () => {
+  contact.scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
 
 for (item of techItems) {
-  item.addEventListener("mouseenter", event => {
+  item.addEventListener('mouseenter', (event) => {
     event.target.style.backgroundColor = colorPicker();
-    event.target.style.boxShadow = "0 0 5px 0 rgba(0, 0, 0, 0.8)";
+    event.target.style.boxShadow = '0 0 5px 0 rgba(0, 0, 0, 0.8)';
 
     // event.target.style.color = 'rgba(0, 0, 0, 1)';
   });
-  item.addEventListener("mouseleave", event => {
-    event.target.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    event.target.style.boxShadow = "none";
-    event.target.style.color = "white";
+  item.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    event.target.style.boxShadow = 'none';
+    event.target.style.color = 'white';
   });
 }
 
 for (item of iconsList) {
-  item.addEventListener("mouseenter", event => {
+  item.addEventListener('mouseenter', (event) => {
     event.target.style.color = colorPicker();
   });
-  item.addEventListener("mouseleave", event => {
-    event.target.style.color = "rgba(0, 0, 0, 0.8)";
+  item.addEventListener('mouseleave', (event) => {
+    event.target.style.color = 'rgba(0, 0, 0, 0.8)';
   });
 }
